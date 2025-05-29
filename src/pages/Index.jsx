0876920acaae7046700d773241dc3f4587e57af1
Gabriel de Souza da Silva <gabrielsouza65115@gmail.com>
2025-05-29 11:26:20 -0300

    fix: Blog e Maps

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -12,6 +12,7 @@ import BackToTopButton from "../components/interactives/BackToTopButton";
 import FloatingWhatsappButton from "../components/interactives/FloatingWhatsappButton";
 import Maps from "../components/sections/Maps";
 import TeamCards from "../components/sections/TeamCards";
+import BlogPosts from "../components/sections/BlogPosts";
 
 export default function Index() {
   // Altere esta constante para: 'default', 'light' ou 'dark'
@@ -28,6 +29,7 @@ export default function Index() {
         influencer={false}
         mesclado={false}
       />
+      {/* <BlogPosts /> */}
       <Features
         colorMode={colorMode}
         defaultFeature={true}
@@ -38,10 +40,14 @@ export default function Index() {
         paragraphsModal={false}
       />
       <About modal={true} showGallery={false} colorMode={colorMode} />
-      <AboutInstagram colorMode={colorMode} instagram={true} socialPrint={true} />
+      <AboutInstagram
+        colorMode={colorMode}
+        instagram={true}
+        socialPrint={true}
+      />
       <Cta colorMode={colorMode} />
       <Steps colorMode={colorMode} />
-      {/* <Maps colorMode={colorMode} /> */}
+      <Maps colorMode={colorMode} />
       <Faq colorMode={colorMode} />
       <FooterSocial
         colorMode={colorMode}
