3ca9c919a4dbadde0aff5ef841c1faefcfd3101f
Gabriel Souza <gabrielsouza65115@gmail.com>
2026-02-13 11:31:48 -0300

    fix: Frase de observação

--- src/content/content.jsx
+++ src/content/content.jsx
@@ -157,7 +157,7 @@ const content = {
             <path d="m16 18 2 2 4-4" />
           </svg>
         ),
-        text: 'Atendimento presencial em Itapema - SC e online para todo Brasil',
+        text: 'Atendimento Online para todo Brasil e presencial em Itapema - SC',
       },
       ctaButtonAriaLabel:
         'Botão para chamada de ação para contato pelo whatsapp',
